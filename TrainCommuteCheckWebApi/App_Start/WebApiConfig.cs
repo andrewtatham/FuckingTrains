242c484f5426f7a8f748f18061bd1c797fdb6664
Andrew Tatham <andrewtatham@gmail.com>
2017-01-05 19:40:58 +0000

    trainy

--- TrainCommuteCheckWebApi/App_Start/WebApiConfig.cs
+++ TrainCommuteCheckWebApi/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
 
 namespace TrainCommuteCheckWebApi
 {
@@ -11,8 +12,10 @@ namespace TrainCommuteCheckWebApi
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new {id = RouteParameter.Optional}
-                );
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new {id = RouteParameter.Optional});
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+               
         }
     }
 }
